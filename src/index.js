8fc59694c83744112c28fd241026853670d6d17b
Aleksey <kedraja@gmail.com>
2019-10-07 21:37:01 +0300

    fix: ref code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = '';
+    let i = 0;
+    while(i < expr.length){
+       let verb = '';
+        if(expr.substr(i , 10) === '**********'){
+            letters += ' ';
+            i+=10;
+            continue;
+        }
+        for(let j = i; j < i+10; j+=2){
+            if(expr.charAt(j) === '0'){
+                continue;
+            }
+            if(expr.charAt(j) === '1' && expr.charAt(j+1) === '1'){
+                verb+='-';
+            }
+            else if(expr.charAt(j) === '1' && expr.charAt(j+1) === '0'){
+                verb+='.';
+            }
+        }
+        if(verb !== ''){
+            letters += MORSE_TABLE[verb];
+        }
+
+        i+=10;
+    }
+    return letters;
 }
+
 
 module.exports = {
     decode
